f09b2a59d5e7710617ce921028d0d5646417831a
Burak-cpu <burakhantepe2005@gmail.com>
2025-03-01 13:53:15 +0300

    added new page

--- shop-page/script.js
+++ shop-page/script.js
@@ -1,1 +1,22 @@
+let cartCount=0;
+function addToCart(){
+    cartCount++;
+    const CartIcon=document.getElementsByClassName("shop-button");
+      if(cartCount>=9){
+        CartIcon.style.backgroundColor="red";
+      }
+      CartIcon.textContent=cartCount;
+}
 
+const liste=document.getElementsByClassName("eklebutonu");
+console.log(liste);
+const listem= Array.from(liste);
+listem.forEach((element) => {
+    element.addEventListener("click",addToCart);
+
+
+});
+
+for(let i=0;i<listem.length;i++){
+    listem[i].addEventListener("click",addToCart)
+}
